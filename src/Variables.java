3398fb421e25beaeb3873dd8c0eedc836ca2afb7
Santiago Castellanos <senasantiagocastellanos@gmail.com>
2024-02-15 11:49:36 -0500

    Primer comentario

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+
+    public static void main (String[] args) {
+        byte edad;
+        edad = 55;
+
+        edad=0;
+
+        System.out.println(edad);
+    }
+
+}
